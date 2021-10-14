73acb2904bc38a04b8228693ec8cacac6a96c026
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2021-10-14 21:48:00 +0500

    Class e'lon qilindi. Database ochildi

---
+++ project1.py
@@ -0,0 +1,15 @@
+
+import mysql.connector
+
+
+class Login:
+    def initial_mes(self):
+        my_db = mysql.connector.connector(
+            host="localhost",
+            user="root",
+            password="sqlok2002",
+            database="w6"
+            )
+
+
+
